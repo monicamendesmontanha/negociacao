c0ce2d8f5c61f47c0cc1bfc1b01b4ae3847fa327
Mônica Mendes <monicamendesmontanha@gmail.com>
2019-02-01 17:02:26 +1100

    refactor import from last week

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,30 +30,19 @@ class NegociacaoController {
 
   importaNegociacoes() {
 
-    let xhr = new XMLHttpRequest();
+    let service = new NegociacaoService();
 
-    xhr.open('GET', 'negociacoes/semana');
-
-    xhr.onreadystatechange = () => {
-
-      if (xhr.readyState == 4) {
-
-        if (xhr.status == 200) {
-
-          JSON.parse(xhr.responseText)
-            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-          this._mensagem.texto = 'Negociações importadas com sucesso!'
-
-        } else {
-          console.log(xhr.responseText);
-          this._mensagem.texto = 'Não foi possível obter as negociações da semana.';
-        }
+    service.obterNegociacoesDaSemana((erro, negociacoes) => {
+      if (erro) {
+        this._mensagem.texto = erro;
+        return;
       }
 
-    };
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+      this._mensagem.texto = 'Negociações importadas com sucesso.';
 
-    xhr.send();
+    });
+
   }
 
   apaga() {
